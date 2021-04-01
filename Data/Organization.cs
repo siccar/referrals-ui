85b0a76b5af6fe3b0ad1b9cd61794cf932b9a94b
Chris Hunter <chris@wallet.services>
2021-04-01 19:57:28 +0100

    Auth in progress

---
+++ Data/Organization.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Data
+{
+    public class Organization
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public int CharityNumber { get; set; }
+        public IEnumerable<string> ServicesIds { get; set; }
+        public string Url { get; set; }
+    }
+}
