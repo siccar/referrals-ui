96b8aa3c5979d570c5e7e7c29c51461b4665f7cd
Chris Hunter <chris@wallet.services>
2021-04-02 11:52:38 +0100

    #3279 - Setup Client and API auth

---
+++ Identity/IIdentityService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Identity
+{
+    public interface IIdentityService
+    {
+        bool IsUserAuthenticated();
+    }
+}
---
+++ Identity/IdentityService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Identity
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly IHttpContextAccessor _context;
+
+        public IdentityService(IHttpContextAccessor context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public bool IsUserAuthenticated()
+        {
+            var isUserAuthenticated = _context.HttpContext.User.Identity.IsAuthenticated;
+            return isUserAuthenticated;
+        }
+    }
+}
