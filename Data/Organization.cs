b2d5fa984719775a85548b5df46b6b8dc0f7126a
Stewart Pavitt <stewart.pavitt@gmail.com>
2021-04-06 14:42:21 +0100

    Added the ability to manage requests

--- Data/Organization.cs
+++ Data/Organization.cs
@@ -11,7 +11,6 @@ namespace OpenReferralPOV.Data
         {
             RequestToJoinStatusMessage = "Request to join";
         }
-
         public string Id { get; set; }
         public string Description { get; set; }
         public string Name { get; set; }
