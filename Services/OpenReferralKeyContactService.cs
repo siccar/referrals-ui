440580eca8c1033735675550917fd3e74b29f35d
Stewart Pavitt <stewart.pavitt@gmail.com>
2021-04-06 09:31:01 +0100

    #3300 Cleaned up some of the layout, added basic structure for Key contacts

---
+++ Services/OpenReferralKeyContactService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using OpenReferralPOV.Data;
+using OpenReferralPOV.Services.HttpClientAdapter;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Services
+{
+    public class OpenReferralKeyContactService : IOpenReferralKeyContactService
+    {
+        private IHttpClientAdapter _httpClientAdapter;
+        private readonly string _ApiBaseAddress = string.Empty;
+
+        public OpenReferralKeyContactService(IHttpClientAdapter httpClientAdapter, IConfiguration configuration)
+        {
+            _httpClientAdapter = httpClientAdapter;
+            _ApiBaseAddress = configuration["ORApi:BaseUrl"];
+        }
+
+        public async Task<IEnumerable<KeyContact>> GetAsync()
+        {
+            var responseString = await _httpClientAdapter.GetAsync(new Uri($"{ _ApiBaseAddress}/KeyContacts"));
+            var keycontacts = JsonConvert.DeserializeObject<IEnumerable<KeyContact>>(responseString);
+
+            return keycontacts;
+        }
+
+        public async Task<KeyContact> AddKeyContact(KeyContact keycontact)
+        {
+            var responseString = await _httpClientAdapter.PostAsync(new Uri($"{ _ApiBaseAddress}/KeyContacts"), keycontact);
+            var addedKeyContact = JsonConvert.DeserializeObject<KeyContact>(responseString);
+            return addedKeyContact;
+        }
+    }
+}
