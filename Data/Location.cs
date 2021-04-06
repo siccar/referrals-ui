04e916696bb1dd1a922c0ba4daeee2c4cd478ed7
Iria Poncela Blanco <iria@siccar.net>
2021-04-06 16:27:01 +0100

    - Data models for Service added, it may need updating. -Added menu link

---
+++ Data/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Data
+{
+    public class Location
+    {
+        public string Description { get; set; }
+        public string Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string> Physical_Addresses_Ids { get; set; }
+        public IEnumerable<string> Service_At_Locations_Ids { get; set; }
+    }
+}
