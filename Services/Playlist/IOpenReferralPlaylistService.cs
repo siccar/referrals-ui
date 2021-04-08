c795356240c0680e0ae752380e23ee95d5aa7c81
Stewart Pavitt <stewart.pavitt@gmail.com>
2021-04-08 13:16:20 +0100

    #3272 - Basic playlist features added

---
+++ Services/Playlist/IOpenReferralPlaylistService.cs
@@ -0,0 +1,15 @@
+﻿using OpenReferralPOV.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Services
+{
+    public interface IOpenReferralPlaylistService
+    {
+        Task<Playlist> Update(Playlist playlist);
+
+        Task<Playlist> Get();
+    }
+}
