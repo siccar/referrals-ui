97d056bd5e13ac9a39d44f898a1b075de5d61138
Stewart Pavitt <stewart.pavitt@gmail.com>
2021-04-09 12:22:03 +0100

    Added basic search - not completed

---
+++ Data/ServiceFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Data
+{
+    public class ServiceFilter
+    {
+        public ServiceFilter()
+        {
+            Categories = new List<string>();
+        }
+        public string Postcode { get; set; }
+
+        public List<string> Categories { get; set; }
+
+        public string SelectedCategory { get; set; }
+
+        public bool SearchForOrgansation { get; set; }
+    }
+}
