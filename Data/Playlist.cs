ad657caf0bbe79c02602c795b596a7891deb79b0
Stewart Pavitt <stewart.pavitt@gmail.com>
2021-04-09 12:22:03 +0100

    Added basic search - not completed

--- Data/Playlist.cs
+++ Data/Playlist.cs
@@ -2,6 +2,10 @@
 {
     public class Playlist
     {
+        public Playlist()
+        {
+            Services = new string[] { };
+        }
         public string UserId { get; set; }
         public string[] Services { get; set; }
     }
---
+++ Services/ServiceFilter/OpenReferralMockServiceFilterService .cs
@@ -0,0 +1,26 @@
+﻿using OpenReferralPOV.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Services
+{
+    public class OpenReferralMockServiceFilterService : IOpenReferralServiceFilterService
+    {
+        public async Task<ServiceFilter> GetSearchFIlter()
+        {
+            return new ServiceFilter()
+            {
+                Postcode = string.Empty,
+                SelectedCategory = string.Empty,
+                Categories = new List<string>
+                {
+                    "Category A",
+                    "Category B",
+                    "Category C",
+                }
+            };
+        }
+    }
+}
