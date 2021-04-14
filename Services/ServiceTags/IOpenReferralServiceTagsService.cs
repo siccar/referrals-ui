e132a640c93aff0176da9a94e29a3eb7938c9404
Iria Poncela Blanco <iria@siccar.net>
2021-04-14 14:17:09 +0100

    WIP: service added for serviceTags, startup modified and model added

---
+++ Services/ServiceTags/IOpenReferralServiceTagsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenReferralPOV.Data.Enums;
+using OpenReferralPOV.Data;
+
+namespace OpenReferralPOV.Services.ServiceTags
+{
+    public interface IOpenReferralServiceTagsService
+    {
+        Task<Data.ServiceTags> Update(Data.ServiceTags serviceTags);
+
+        Task<Data.ServiceTags> Get(string serviceId);
+    }
+}
