8f333a094667801b1d64d04bf4796480edb9bfce
Chris Hunter <chris@wallet.services>
2021-04-14 15:13:52 +0100

    #3329 - Add validation again

--- Data/PhysicalAddress.cs
+++ Data/PhysicalAddress.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +17,10 @@ namespace OpenReferralPOV.Data
         public string Country { get; set; }
         public string Id { get; set; }
         public string LocationId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Postcode must be populated")]
+        [RegularExpression(@"(GIR 0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9][0-9]?)|(([A-Z-[QVX]][0-9][A-HJKSTUW])|([A-Z-[QVX]][A-Z-[IJZ]][0-9][ABEHMNPRVWXY])))) [0-9][A-Z-[CIKMOV]]{2})",
+         ErrorMessage = "The Postcode must be a valid UK Postcode.")]
         public string Postal_Code { get; set; }
         public string State_Province { get; set; }
     }
