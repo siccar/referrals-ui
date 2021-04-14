eb3291626ad4219471ffb82f814a54f9db93450b
Iria Poncela Blanco <iria@siccar.net>
2021-04-14 12:01:44 +0100

    WIP: added model and modified enum

---
+++ Data/ServiceTags.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenReferralPOV.Data
+{
+    public class ServiceTags
+    {
+        public ServiceTags()
+        {
+            Tags = new List<string>();
+        }
+        public string ServiceId { get; set; }
+        public List<int> Tags { get; set; }
+    }
+}
