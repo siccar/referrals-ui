58b80db806905c0700f361256fbb4da6a5a88f0f
Chris Hunter <chris@wallet.services>
2021-04-18 13:31:27 +0100

    #3375 - Add in service availibility

--- Data/RegularSchedule.cs
+++ Data/RegularSchedule.cs
@@ -7,15 +7,19 @@ namespace OpenReferralPOV.Data
 {
     public class RegularSchedule
     {
+        public RegularSchedule()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        public string Id { get; set; }
         public string ByDay { get; set; }
-        public string ByMonthDay { get; set; }
+        public DateTime Opens_At { get; set; }
         public DateTime Closes_At { get; set; }
+        public string ByMonthDay { get; set; }
         public string Description { get; set; }
         public string DtStart { get; set; }
         public string Freq { get; set; }
-        public string Id { get; set; }
         public string Interval { get; set; }
-        public DateTime Opens_At { get; set; }
         public Service Service { get; set; }
         public ServiceAtLocation Service_At_Location { get; set; }
         public DateTime Valid_From { get; set; }
