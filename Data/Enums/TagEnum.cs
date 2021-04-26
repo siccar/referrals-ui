78e93de6c4f847dc766cd1c60fc2449485bd9580
Chris Hunter <chris@wallet.services>
2021-04-26 15:09:22 +0100

    Add tags

--- Data/Enums/TagEnum.cs
+++ Data/Enums/TagEnum.cs
@@ -6,11 +6,43 @@ using System.Threading.Tasks;
 namespace OpenReferralPOV.Data.Enums
 {
     public enum TagEnum : int
-    { 
-        Tag1 = 1,
-        Tag2 = 2,
-        Tag3 = 3,
-        Tag4 = 4,
-        Tag5 = 5
+    {
+        Advice = 1,
+        AmateurSport = 2,
+        Ambulance = 3,
+        AnimalWelfare = 4,
+        ArmedForces = 5,
+        ArtsAndCulture = 6,
+        ChildrenAndYoungPeople = 7,
+        Citizenship = 8,
+        CommunityAndEconomicDevelopment = 9,
+        DomesticViolenceAndSexualAbuse = 10,
+        Education = 11,
+        EmergencyAndReliefServices = 12,
+        EmploymentAndTraining = 13,
+        Environment = 14,
+        FamilyServices = 15,
+        Fire = 16,
+        Food = 17,
+        GrantMakingFoundations = 18,
+        Health = 19,
+        Homelessness = 20,
+        Housing = 21,
+        HumanRights = 22,
+        Infrastructure = 23,
+        MedicalResearch = 24,
+        Police = 25,
+        RefugeesAndMigrants = 26,
+        RelievingPoverty = 27,
+        Religion = 28,
+        ReligiousOrRacialHarmony = 29,
+        ResidentialCare = 30,
+        SavingLives = 31,
+        ScienceAndResearch = 32,
+        ServicesForPeopleWithDisabilities = 33,
+        ServicesForTheElderly = 34,
+        SocialAdvocacy = 35,
+        SocialClubs = 36,
+        Sports = 37
     }
 }
